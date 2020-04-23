1b9e2dd4eb5b7c30ba381f840d3b3deb5af7e622
Ross MacArthur <ross@macarthur.io>
2020-04-23 22:46:11 +0200

    Many fixes and add linting with eslint

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const core = require('@actions/core');
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
 try {
-  const machine = core.getInput('machine');
-  const username = encodeURI(core.getInput('username'));
-  const password = encodeURI(core.getInput('password'));
+  const machine = encodeURI(core.getInput('machine')) || "''";
+  const username = encodeURI(core.getInput('username')) || "''";
+  const password = encodeURI(core.getInput('password')) || "''";
   const line = `machine ${machine} login ${username} password ${password}\n`;
-  const netrc = path.resolve(os.homedir(), '.netrc')
-  fs.writeFile(netrc, line, { 'flag': 'a' }, (error) => {
+  const netrc = path.resolve(os.homedir(), '.netrc');
+  fs.writeFile(netrc, line, { flag: 'a' }, (error) => {
     if (error) throw error;
-    console.log(`wrote ${machine} credentials to ~/.netrc`);
-  })
+    core.info(`wrote ${machine} credentials to ~/.netrc`);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
