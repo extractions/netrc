4b186cfa5a29ecfc07522aa538c0eb4c9236ee1e
Ross MacArthur <ross@macarthur.io>
2020-04-23 14:26:11 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const fs = require('fs')
+const path = require('path')
+
+try {
+  const machine = core.getInput('machine');
+  const username = encodeURI(core.getInput('username'));
+  const password = encodeURI(core.getInput('password'));
+  const line = `machine ${machine} login ${username} password ${password}\n`;
+  const netrc = path.resolve(os.homedir(), '.netrc')
+  fs.writeFile(netrc, line, { 'flag': 'a' }, (error) => {
+    if (error) throw error;
+    console.log(`wrote ${machine} credentials to ~/.netrc`);
+  })
+} catch (error) {
+  core.setFailed(err.message);
+}
