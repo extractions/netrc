487491cfcbcb52705fef1cdac86ab5573da441e0
Ross MacArthur <ross@macarthur.io>
2021-05-11 22:47:41 +0200

    Error on insecure file ownership or permissions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,29 @@
 import * as core from "@actions/core";
 import * as os from "os";
-import * as fs from "fs";
+import { constants, promises as fs } from "fs";
 import * as path from "path";
+
+async function checkPermissions(p: string): Promise<void> {
+  if (process.platform != "linux" && process.platform != "darwin") {
+    return;
+  }
+  try {
+    const stat = await fs.stat(p);
+    if (stat.uid != process.getuid()) {
+      throw new Error("~/.netrc file owner does not match current user");
+    }
+    if (stat.mode & (constants.S_IRWXG | constants.S_IRWXO)) {
+      throw new Error(
+        "~/.netrc access too permissive: " +
+          "access permissions must restrict access to only the owner"
+      );
+    }
+  } catch (err) {
+    if (err.code != "ENOENT") {
+      throw err;
+    }
+  }
+}
 
 async function main() {
   try {
@@ -10,10 +32,8 @@ async function main() {
     const password = encodeURI(core.getInput("password")) || "''";
     const line = `machine ${machine} login ${username} password ${password}\n`;
     const netrc = path.resolve(os.homedir(), ".netrc");
-    fs.writeFile(netrc, line, { flag: "a" }, (err) => {
-      if (err) throw err;
-      core.info(`Setup ~/.netrc credentials for ${machine}`);
-    });
+    await checkPermissions(netrc);
+    await fs.writeFile(netrc, line, { flag: "a", mode: 0o600 });
   } catch (err) {
     core.setFailed(err.message);
   }
