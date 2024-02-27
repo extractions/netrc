6c94ab9966b980018014cf5e2c0b1db10c4dc76d
Robbe Van Petegem <git@robbevp.be>
2024-02-27 23:10:54 +0100

    Don't set action as failed when file was already deleted

--- src/remove.ts
+++ src/remove.ts
@@ -8,10 +8,16 @@ async function main() {
     const netrc = path.resolve(os.homedir(), ".netrc");
     await fs.unlink(netrc);
   } catch (err) {
+    if (isErrnoException(err) && err.code == "ENOENT") return;
     if (err instanceof Error) {
       core.setFailed(err.message);
     }
   }
+}
+
+function isErrnoException(e: unknown): e is NodeJS.ErrnoException {
+  if ("code" in (e as any)) return true;
+  else return false;
 }
 
 main();
